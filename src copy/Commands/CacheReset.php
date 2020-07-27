bc873386a6ac7d8b5c20f461c873eb07155ef69f
cafelaptrinh <cafelaptrinh2@gmail.com>
2020-07-27 08:01:43 +0700

    add spatie

---
+++ src copy/Commands/CacheReset.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace CafeLT\Permission\Commands;
+
+use Illuminate\Console\Command;
+use CafeLT\Permission\PermissionRegistrar;
+
+class CacheReset extends Command
+{
+    protected $signature = 'permission:cache-reset';
+
+    protected $description = 'Reset the permission cache';
+
+    public function handle()
+    {
+        if (app(PermissionRegistrar::class)->forgetCachedPermissions()) {
+            $this->info('Permission cache flushed.');
+        } else {
+            $this->error('Unable to flush cache.');
+        }
+    }
+}
---
+++ src copy/Middlewares/RoleOrPermissionMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace CafeLT\Permission\Middlewares;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use CafeLT\Permission\Exceptions\UnauthorizedException;
+
+class RoleOrPermissionMiddleware
+{
+    public function handle($request, Closure $next, $roleOrPermission)
+    {
+        if (Auth::guest()) {
+            throw UnauthorizedException::notLoggedIn();
+        }
+
+        $rolesOrPermissions = is_array($roleOrPermission)
+            ? $roleOrPermission
+            : explode('|', $roleOrPermission);
+
+        if (! Auth::user()->hasAnyRole($rolesOrPermissions) && ! Auth::user()->hasAnyPermission($rolesOrPermissions)) {
+            throw UnauthorizedException::forRolesOrPermissions($rolesOrPermissions);
+        }
+
+        return $next($request);
+    }
+}
